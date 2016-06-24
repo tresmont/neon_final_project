7a722fda4c2eda0e764fc170e6d0d7ae01212465
Tres Montano <tresmont@gmail.com>
2016-06-24 11:01:41 -0600

    Adding code for buffering points to polygon

---
+++ scripts/bufferPoints.R
@@ -0,0 +1,57 @@
+# Function to create a buffered ploygon from points
+
+library(sp)
+
+bufferPoly <- function(site, ptShp, radius){
+  if(missing(ptShp)){
+    stop("input point polygon is required")
+  }
+  if(missing(radius)){
+    #set the radius for the plots
+    radius <- 20 #radius in meters
+  } else {
+    #set the radius for the plots
+    radius <- radius #radius in meters
+  }
+  
+  #define the plot boundaries based upon the plot radius. 
+  #NOTE: this assumes that plots are oriented North and are not rotated. 
+  #If the plots are rotated, you'd need to do additional math to find 
+  #the corners.
+  yPlus <- ptShp$northing+radius
+  xPlus <- ptShp$easting+radius
+  yMinus <- ptShp$northing-radius
+  xMinus <- ptShp$easting-radius
+  
+  #Extract the plot ID information. NOTE: because we set
+  #stringsAsFactor to false above, we can import the plot 
+  #ID's using the code below. If we didn't do that, our ID's would 
+  #come in as factors by default. 
+  #We'd thus have to use the code ID=as.character(centroids$Plot_ID) 
+  ID <- ptShp$Plot_ID
+  prj_info <- ptShp@proj4string@projargs
+    
+    
+  #calculate polygon coordinates for each plot centroid. 
+  square <- cbind(xMinus,yPlus, xPlus,yPlus, xPlus,yMinus, xMinus,yMinus,xMinus,yPlus,xMinus,yPlus)
+  
+  #create spatial polygons
+  polys <- SpatialPolygons(mapply(function(poly, id) {
+    xy <- matrix(poly, ncol=2, byrow=TRUE)
+    Polygons(list(Polygon(xy)), ID=id)
+  }, split(square, row(square)), ID),proj4string=CRS(as.character(prj_info)))
+  
+  
+  # Create SpatialPolygonDataFrame -- this step is required to output multiple polygons.
+  polys.df <- SpatialPolygonsDataFrame(polys, data.frame(id=ID, row.names=ID))
+  
+  #write out the data
+  # outfile <- sprintf("%s_%sm", site, radius)
+  
+  writeOGR(polys.df, 'output', paste(site, radius, sep = "_"), 'ESRI Shapefile',
+           overwrite_layer=TRUE)
+  
+}
+
+
+
